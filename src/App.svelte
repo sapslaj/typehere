e5caa841965b5689392f40e97c8d9ec2b57ce81a
Justin Roberson <saps.laj@gmail.com>
2021-10-13 10:09:05 -0400

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { writable } from "svelte/store";
+
+  const storedContent = localStorage.content || "";
+  const content = writable(storedContent);
+  content.subscribe(value => localStorage.content = value);
+</script>
+
+<style>
+  .container {
+    height: 100%;
+    width: 100%;
+    overflow: hidden;
+    font-family: monospace;
+  }
+  .editor {
+    width: 100%;
+    height: 100%;
+    font-size: 1rem;
+    padding: 1rem;
+    margin: 0;
+    resize: none;
+    border: 0;
+  }
+</style>
+
+<div class="container">
+  <textarea class="editor" bind:value={$content}></textarea>
+</div>
